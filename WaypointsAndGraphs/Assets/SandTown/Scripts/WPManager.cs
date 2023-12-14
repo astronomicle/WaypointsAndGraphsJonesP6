991498c73166e4c975fb4df219fd542bb757a632
astronomicle <719168@student.dusd.net>
2023-12-13 16:32:56 -0800

    did another part
    i finished a* with waypoints part 1 and now on the next item

---
+++ WaypointsAndGraphs/Assets/SandTown/Scripts/WPManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public struct Link
+{
+    public enum direction { UNI, BI }
+    public GameObject node1;
+    public GameObject Node2;
+    public direction dir;
+}
+
+public class WPManager : MonoBehaviour
+{
+    public GameObject[] waypoints;
+    public Link[] links;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
