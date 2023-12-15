9bbdc697d774bd910641b47950bba698e6dd4638
astronomicle <719168@student.dusd.net>
2023-12-14 16:49:49 -0800

    finsihed but somethig wronk
    I completed all of the sections and it all works except for the tank in the sand town, it doesn't move, a problem for later

---
+++ WaypointsAndGraphs/Assets/SandTown/FollowWPs.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class FollowWPs : MonoBehaviour
+{
+    Transform goal;
+    float speed = 5.0f;
+    float accuracy = 5.0f;
+    float rotSpeed = 2.0f;
+
+    public GameObject wpManager;
+    GameObject[] wps;
+    GameObject currentNode;
+    int currentWP = 0;
+    Graph g;
+
+    void Start()
+    {
+        wps = wpManager.GetComponent<WPManager>().waypoints;
+        g = wpManager.GetComponent<WPManager>().graph;
+        currentNode = wps[0];
+
+        Invoke("GoToRuin", 2);
+    }
+
+    public void GoToHeli()
+    {
+        g.AStar(currentNode, wps[0]);
+        currentWP = 0;
+    }
+
+    public void GoToRuin()
+    {
+        g.AStar(currentNode, wps[9]);
+        currentWP = 0;
+    }
+
+    public void GoToFactory()
+    {
+        g.AStar(currentNode, wps[3]);
+        currentWP = 0;
+    }
+
+    void LateUpdate()
+    {
+        if (g.pathList.Count == 0 || currentWP == g.pathList.Count)
+            return;
+
+        if (Vector3.Distance(g.pathList[currentWP].getId().transform.position, this.transform.position) < accuracy)
+        {
+            currentNode = g.pathList[currentWP].getId();
+            currentWP++;
+        }
+
+        if (currentWP < g.pathList.Count)
+        {
+            goal = g.pathList[currentWP].getId().transform;
+            Vector3 lookAtGoal = new Vector3(goal.position.x, this.transform.position.y,goal.position.z);
+            Vector3 direction = lookAtGoal - this.transform.position;
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), Time.deltaTime * rotSpeed);
+
+            this.transform.Translate(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
--- WaypointsAndGraphs/Assets/SandTown/Graphs/Graph.cs
+++ WaypointsAndGraphs/Assets/SandTown/Graphs/Graph.cs
@@ -7,7 +7,7 @@ public class Graph
 {
     List<Edge> edges = new List<Edge>();
     List<Node> nodes = new List<Node>();
-    List<Node> pathList = new List<Node>();
+    public List<Node> pathList = new List<Node>();
 
     public Graph() { }
 
@@ -42,6 +42,12 @@ public class Graph
 
     public bool AStar (GameObject startId, GameObject endId)
     {
+        if(startId == endId)
+        {
+            pathList.Clear();
+            return false;
+        }
+
         Node start = FindNode(startId);
         Node end = FindNode(endId);
 
